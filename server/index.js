5c72be0f0ae7affdf6a61337d6a361c8c1d8e913
Chris Gallagher <12512900+gallagherrchris@users.noreply.github.com>
2020-05-06 08:49:45 -0400

    Changed server roots.

--- server/index.js
+++ server/index.js
@@ -5,17 +5,19 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+const appRoot = path.join(__dirname, '..', 'build');
+
+app.use(favicon(path.join(appRoot, 'favicon.ico')));
+app.use(express.static(path.join(__dirname, '..')));
+app.use(express.static(path.join(appRoot)));
 
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(appRoot, 'index.html'));
 });
 
 app.listen(port);
