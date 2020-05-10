f4415538e37dc9a5c3880b99dafd58747eb086eb
Chris Gallagher <12512900+gallagherrchris@users.noreply.github.com>
2020-05-10 10:49:05 -0400

    Added ability to create a new team.

--- src/components/lobby.js
+++ src/components/lobby.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './lobby.scss';
 import GameResults from './gameResults';
+
+const NewTeam = ({ changeTeam }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [teamName, setTeamName] = useState('');
+
+  const submitTeamName = (event) => {
+    changeTeam(teamName, event);
+    setIsEditing(false);
+    setTeamName('');
+  };
+
+  return isEditing ? (
+    <form onSubmit={submitTeamName}>
+      <label htmlFor="team-name">
+        Team Name
+        <input id="team-name" type="text" value={teamName} onChange={({ target }) => setTeamName(target.value)} required />
+      </label>
+      <div className="button-container">
+        <button type="button" onClick={() => setIsEditing(false)}>Cancel</button>
+        <button type="submit">Create Team</button>
+      </div>
+    </form>
+  ) : <button type="button" onClick={() => setIsEditing(true)}>Create new team</button>;
+};
 
 const Lobby = ({ sendMessage, gameState: { teams, user, gameResults } }) => {
 
@@ -19,7 +43,7 @@ const Lobby = ({ sendMessage, gameState: { teams, user, gameResults } }) => {
       <section className="team-container">
         {teams.map(team => (
           <article className="team" key={team.name}>
-            {user.team === team.name ? <span>Current Team</span> : <button type="button" onClick={changeTeam.bind(null, team.name)}>Switch Team</button>}
+            {user.team === team.name ? <span>Current Team</span> : <button type="button" onClick={changeTeam.bind(null, team.name)}>Join Team</button>}
             <p className="name">{team.name}</p>
             <p className="players">
               {team.players.map(player => (
@@ -28,6 +52,7 @@ const Lobby = ({ sendMessage, gameState: { teams, user, gameResults } }) => {
             </p>
           </article>
         ))}
+        <article className="create-team"><NewTeam changeTeam={changeTeam}/></article>
       </section>
       <button className="start" type="button" onClick={startGame}>Start Game</button>
       <GameResults results={gameResults} />
--- src/components/register.js
+++ src/components/register.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 const Register = ({ sendMessage }) => {
   const [inputs, setInputs] = useState({
