5c25b218edae4bddc913857f869688e29d1157e6
Chris Gallagher <12512900+gallagherrchris@users.noreply.github.com>
2020-05-16 01:08:49 -0400

    Removed old component

--- src/components/lobby.js
+++ src/components/lobby.js
@@ -1,31 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './lobby.scss';
 import GameResults from './gameResults';
 import ChangeTeam from './changeTeam';
-
-const NewTeam = ({ changeTeam }) => {
-  const [isEditing, setIsEditing] = useState(false);
-  const [teamName, setTeamName] = useState('');
-
-  const submitTeamName = (event) => {
-    changeTeam(teamName, event);
-    setIsEditing(false);
-    setTeamName('');
-  };
-
-  return isEditing ? (
-    <form onSubmit={submitTeamName}>
-      <label htmlFor="team-name">
-        Team Name
-        <input id="team-name" type="text" value={teamName} onChange={({ target }) => setTeamName(target.value)} required />
-      </label>
-      <div className="button-container">
-        <button type="button" onClick={() => setIsEditing(false)}>Cancel</button>
-        <button type="submit">Create Team</button>
-      </div>
-    </form>
-  ) : <button type="button" onClick={() => setIsEditing(true)}>Create new team</button>;
-};
 
 const Lobby = ({ sendMessage, gameState: { teams, user, gameResults } }) => {
 
